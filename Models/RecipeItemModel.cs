02b43c54059d3a12a4a183d25b3d4d04d12dfcb8
jonatasgpj <jonataasgoomes@gmail.com>
2025-04-13 17:09:57 -0300

    começando a implementação das classes

---
+++ Models/RecipeItemModel.cs
@@ -0,0 +1,12 @@
+﻿namespace recipeAPI.Models
+{
+    public class RecipeItemModel
+    {
+        public int Id { get; set; }
+        public IngredientModel Ingredient { get; set; }
+        public int Quantity { get; set; }
+
+
+
+    }
+}
---
+++ Models/RecipeModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json.Serialization;
+
+namespace recipeAPI.Models
+{
+    public class RecipeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [JsonIgnore]
+        public string Instructions { get; set; }
+        public ICollection<RecipeItemModel> Ingredients { get; set; }
+
+
+    }
+}
