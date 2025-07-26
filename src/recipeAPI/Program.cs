51579ca1fb8b3a0c6368776b19e058db6d666979
jonatasgpj <jonataasgoomes@gmail.com>
2025-07-26 14:10:11 -0300

    commit final

--- src/recipeAPI/Program.cs
+++ src/recipeAPI/Program.cs
@@ -6,6 +6,7 @@ using OpenTelemetry;
 using OpenTelemetry.Trace;
 using OpenTelemetry.Logs;
 using OpenTelemetry.Resources;
+using OpenTelemetry.Metrics;
 
 
 
@@ -26,6 +27,25 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+
+builder.Services.AddOpenTelemetry()
+    .WithTracing(tracing => tracing
+        // The rest of your setup code goes here
+        .AddOtlpExporter()
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation())
+    .WithMetrics(metrics => metrics
+        // The rest of your setup code goes here
+        .AddPrometheusExporter()
+        .AddAspNetCoreInstrumentation()
+        .AddHttpClientInstrumentation());
+
+builder.Logging.AddOpenTelemetry(logging => {
+    // The rest of your setup code goes here
+    logging.AddOtlpExporter();
+});
+
+
 builder.Logging.AddConsole();
 builder.Logging.AddOpenTelemetry(options =>
 {
@@ -33,9 +53,9 @@ builder.Logging.AddOpenTelemetry(options =>
     options.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("recipeAPI"));
     options.AddOtlpExporter(opt =>
     {
-        opt.Endpoint = new Uri("http://localhost:4317");
+        opt.Endpoint = new Uri("http://loki:3100/otlp/v1/logs");
     });
-});
+}); 
 
 
 
@@ -56,15 +76,16 @@ app.UseSwagger();
 app.UseSwaggerUI();
 //}
 
-// using (var scope = app.Services.CreateScope())
-// {
-//     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//     Console.WriteLine("Aplicando migrations...");
-//     db.Database.Migrate();
-//     Console.WriteLine("Migrations aplicadas com sucesso.");
-// }
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    Console.WriteLine("Aplicando migrations...");
+    db.Database.Migrate();
+    Console.WriteLine("Migrations aplicadas com sucesso.");
+}
 
 app.UseHttpsRedirection();
+app.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 app.UseAuthorization();
 
